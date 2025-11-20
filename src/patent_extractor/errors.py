c71caea280548895171a5bdaae5b546e44d43f08
Ryan-Niksa <ryan@axiomhealthmanagement.com>
2025-11-20 10:14:30 -0500

    refactor: remove remaining docstrings from error classes

--- src/patent_extractor/errors.py
+++ src/patent_extractor/errors.py
@@ -1,28 +1,15 @@
-"""
-Custom exception classes for patent XML extraction.
-"""
-
-
 class PatentExtractorError(Exception):
-    """Base exception for all patent extractor errors."""
-
     pass
 
 
 class FileError(PatentExtractorError):
-    """Raised when file operations fail."""
-
     pass
 
 
 class XMLParseError(PatentExtractorError):
-    """Raised when XML parsing fails."""
-
     pass
 
 
 class ExtractionError(PatentExtractorError):
-    """Raised when doc-number extraction fails."""
-
     pass
 
